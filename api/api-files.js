cb9962bbd980925372b8969fe656597952bc0180
gapmfi-ux <gapmfi@gmail.com>
2026-01-16 17:39:00 +0000

    Create api-files.js

---
+++ api/api-files.js
@@ -0,0 +1,39 @@
+// File Operations API methods
+class FilesAPI {
+  constructor(apiService) {
+    this.api = apiService;
+  }
+
+  async copyLendingTemplate(appNumber, appFolderId) {
+    return this.api.request('copyLendingTemplate', {
+      appNumber: appNumber,
+      appFolderId: appFolderId
+    });
+  }
+
+  async uploadApplicationFile(fileData, appNumber, appFolderId, docType) {
+    return this.api.request('uploadApplicationFile', {
+      fileData: fileData,
+      appNumber: appNumber,
+      appFolderId: appFolderId,
+      docType: docType
+    });
+  }
+
+  async updateApplicationFile(fileData, appNumber, docType, userName) {
+    return this.api.request('updateApplicationFile', {
+      fileData: fileData,
+      appNumber: appNumber,
+      docType: docType,
+      userName: userName
+    });
+  }
+
+  async removeApplicationFile(appNumber, fileName, userName) {
+    return this.api.request('removeApplicationFile', {
+      appNumber: appNumber,
+      fileName: fileName,
+      userName: userName
+    });
+  }
+}
